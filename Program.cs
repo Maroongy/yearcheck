64b988fe8dec56d4d56c62a32566e09f9726e691
Maroongy <vladislav.morozov31@mail.ru>
2024-02-29 17:50:43 +0700

    init project
    исходный

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите год: ");
+        int year = int.Parse(Console.ReadLine());
+
+        if (year % 4 == 0)
+        {
+            Console.WriteLine("Год високосный");
+        }
+        else
+        {
+            Console.WriteLine("Год не високосный");
+        }
+    }
+}
